0b466999f900cdad126b3b7ee299b511f3cc5068
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@github.com/hamzasorani83/Portfolio <hamzasorani83@gmail.com>
2023-01-24 12:57:46 +0300

    server

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+const port = process.env.PORT || 3001
+
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
+    console.log('JSON Server is running')
 })
